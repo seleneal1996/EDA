29247b4f90727cded2b59885f7f00a7cc526a33c
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-07 05:27:39 +0000

    selection

---
+++ LAB1/Selection/selection.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void selectionSort(int *arr,int size)
+{
+  int temp;
+  for(int i = 0 ; i < size - 1 ;  i++ )
+  {
+    int minimo = i;
+    for ( int j = i+1 ; j < size  ;  j++ )
+    {
+      if( arr[minimo] > arr[j] )
+      minimo = j ;
+    }
+
+    temp = arr[i];
+    arr[i] = arr[minimo];
+    arr[minimo] = temp;
+        
+    }
+}
+
+int main()
+{
+    int input,size;
+    cin >> size;
+    int array[size];
+    for(int i = 0 ; i < size ; i++ )
+    {
+        cin >> input;
+        array[i] = input;
+    }
+    
+    unsigned t0,t1;
+    t0 = clock();
+    selectionSort(array,size);
+    t1 = clock();
+    double time = (double(t1-t0)/CLOCKS_PER_SEC);
+    cout << time << endl;
+    return 0;
+}
---
+++ LAB1/Selection/selection.py
@@ -0,0 +1,26 @@
+import sys
+from time import time
+def SelectionSort(arr):
+  temp = 0
+  arr2 = arr.copy()
+  for i in range(len(arr2)):
+    minimo = i
+    for j in range(i+1,len(arr2)):
+      if arr2[minimo] > arr2[j]:
+        minimo = j
+      temp = arr2[i]
+      arr2[i] = arr2[minimo]
+      arr2[minimo] = temp
+
+archivo = sys.stdin.readlines()
+n = int(archivo[0])
+arr = []
+dato = archivo[1].split()
+for i in range (n):
+    arr.append(int(dato[i]))
+start = time()
+SelectionSort(arr)
+finish = time()
+print(finish-start,end = '')
+
+
